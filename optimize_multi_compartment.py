190e0cf8fa836c460d5d600fd9ebe61235f8e77b
Daniele Linaro <danielelinaro@gmail.com>
2017-10-11 16:32:43 -0400

    Minor changes to optimize_multi_compartment.py

--- optimize_multi_compartment.py
+++ optimize_multi_compartment.py
@@ -7,7 +7,7 @@ import shutil
 import pickle
 import argparse as arg
 import numpy as np
-#import matplotlib.pyplot as plt
+from numpy.random import poisson
 import bluepyopt
 import dlopt
 
@@ -18,16 +18,11 @@ def get_responses(evaluator, individuals, filename=None):
         responses.append(evaluator.run_protocols(protocols=evaluator.fitness_protocols.values(),
                                                  param_values=evaluator.param_dict(individual)))
     if not filename is None:
-        with open(filename,'w') as fid:
-            pickle.dump(responses,fid)
+        pickle.dump(responses,open(filename,'w'))
+
     return responses
 
 
-def save_individuals(individuals, filename):
-    with open(filename,'w') as fid:
-        pickle.dump(individuals,fid)
-
-        
 def main():
     parser = arg.ArgumentParser(description='Optimize a multi-compartment neuron model.')
     parser.add_argument('swc_file', type=str, action='store', help='SWC file')
@@ -71,11 +66,19 @@ def main():
             print('%s: %s: no such file.' % (os.path.basename(sys.argv[0]),f))
             sys.exit(1)
 
-    now = time.gmtime(time.time())
-    output_folder = '%d%02d%02d%02d%02d%02d_%s' % (now.tm_year,now.tm_mon,now.tm_mday,
-                                                   now.tm_hour,now.tm_min,now.tm_sec,
-                                                   cell_name)
-    os.mkdir(output_folder, 0755)
+    time.sleep(poisson(10))
+    while True:
+        now = time.gmtime(time.time())
+        output_folder = '%d%02d%02d%02d%02d%02d_%s' % (now.tm_year,now.tm_mon,now.tm_mday,
+                                                       now.tm_hour,now.tm_min,now.tm_sec,
+                                                       cell_name)
+        if os.path.isdir(output_folder):
+            nsec = poisson(5)
+            print('Output folder [%s] exists: sleeping for %d seconds...' % (output_folder,nsec))
+            time.sleep(nsec)
+        else:
+            os.mkdir(output_folder, 0755)
+            break
 
     for f in filenames.values():
         if os.path.isfile(f):
@@ -88,53 +91,25 @@ def main():
     evaluator = dlopt.evaluator.create(cell_name, filenames, config_dir=args.config_dir)
     print(evaluator.cell_model)
 
+    seed = int(time.time())
     optimisation = bluepyopt.optimisations.DEAPOptimisation(evaluator=evaluator,
                                                             offspring_size=args.population_size,
-                                                            use_scoop=True)
+                                                            use_scoop=True,
+                                                            seed=seed)
 
     final_pop,hall_of_fame,logbook,history = optimisation.run(max_ngen=args.num_generation)
  
     best_ind = hall_of_fame[0]
     best_ind_dict = evaluator.param_dict(best_ind)
 
-    print('The best values obtained with the optimization are:')
-    for k,v in best_ind_dict.iteritems():
-        print('%30s = %9.1e' % (k,v))
-
-    #### let's simulate the responses of the final population
+    #### let's simulate the responses of the hall of fame population
     responses = get_responses(evaluator, hall_of_fame, output_folder+'/hall_of_fame_responses.pkl')
-    #get_responses(evaluator, final_pop, output_folder+'/final_population_responses.pkl')
-    save_individuals(hall_of_fame, output_folder+'/hall_of_fame.pkl')
-    save_individuals(final_pop, output_folder+'/final_population.pkl')
-    with open(output_folder+'/evaluator.pkl','w') as fid:
-        pickle.dump(evaluator,fid)
-    with open(output_folder+'/logbook.pkl','w') as fid:
-        pickle.dump(logbook,fid)
-    with open(output_folder+'/history.pkl','w') as fid:
-        pickle.dump(history,fid)
-    
-    #### let's plot the results
-    #for resp in responses:
-    #    for k,v in resp.iteritems():
-    #        if 'soma' in k:
-    #            plt.plot(v['time'],v['voltage'],linewidth=1)
-    #for k,v in responses[0].iteritems():
-    #    if 'soma' in k:
-    #        plt.plot(v['time'],v['voltage'],'k',linewidth=2,label='Best individual')
-    #plt.xlabel('Time (ms)')
-    #plt.ylabel(r'$V_m$ (mV)')
-    #plt.legend(loc='best')
-    #plt.show()
-    
-    #params = {name: [] for name in evaluator.param_names}
-    #for individual in final_pop:
-    #    for k,v in evaluator.param_dict(individual):
-    #        params[k].append(v)
-    #cnt = 0.75
-    #for k,v in params.iteritems():
-    #    plt.plot(cnt+0.5*rnd(size=len(v)),v,'ko')
-    #    cnt += 1
-    #plt.show()
+    pickle.dump(hall_of_fame, open(output_folder+'/hall_of_fame.pkl','w'))
+    pickle.dump(final_pop, open(output_folder+'/final_population.pkl','w'))
+    pickle.dump(evaluator, open(output_folder+'/evaluator.pkl','w'))
+    pickle.dump(logbook, open(output_folder+'/logbook.pkl','w'))
+    pickle.dump(history, open(output_folder+'/history.pkl','w'))
+    pickle.dump(seed, open(output_folder+'/seed.pkl','w'))
 
 if __name__ == '__main__':
     main()
