2581b1beda49c91130e60012376f5506a0b58877
Daniele Linaro <danielelinaro@gmail.com>
2019-04-25 12:05:02 +0200

    Modified pick_cells.py to use SCOOP.

--- pick_cells.py
+++ pick_cells.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import os
 import sys
 import copy
 import json
+import glob
 import pickle
 import argparse as arg
 import numpy as np
@@ -11,28 +14,7 @@ import matplotlib.pyplot as plt
 
 import efel
 from neuron import h
-from scoop import futures
 from current_step import inject_current_step
-
-
-class ColorFactory:
-    RED = '\033[91m'
-    GREEN = '\033[92m'
-    BLUE = '\033[94m'
-    YELLOW = '\033[93m'
-    ENDC = '\033[0m'
-    def __init__(self):
-        pass
-    def __color__(self, col, str):
-        return col + str + self.ENDC
-    def red(self, str):
-        return self.__color__(self.RED, str)
-    def green(self, str):
-        return self.__color__(self.GREEN, str)
-    def blue(self, str):
-        return self.__color__(self.BLUE, str)
-    def yellow(self, str):
-        return self.__color__(self.YELLOW, str)
 
 
 def equal_sections(sec_a, sec_b, h, soma_a=None, soma_b=None):
@@ -93,6 +75,29 @@ def equal_sections(sec_a, sec_b, h, soma_a=None, soma_b=None):
     return True
 
 
+class ColorFactory:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    ENDC = '\033[0m'
+    def __init__(self):
+        pass
+    def __color__(self, col, str):
+        return col + str + self.ENDC
+    def red(self, str):
+        return self.__color__(self.RED, str)
+    def green(self, str):
+        return self.__color__(self.GREEN, str)
+    def blue(self, str):
+        return self.__color__(self.BLUE, str)
+    def yellow(self, str):
+        return self.__color__(self.YELLOW, str)
+
+
+colors = ColorFactory()
+
+
 def dump_parameters(parameters,default_parameters,evaluator,filename):
     param_dict = evaluator.param_dict(parameters)
     default_parameters_copy = copy.deepcopy(default_parameters)
@@ -103,13 +108,68 @@ def dump_parameters(parameters,default_parameters,evaluator,filename):
     json.dump(default_parameters_copy,open(filename,'w'),indent=4)
 
 
-def main():
+argsort = lambda seq: sorted(range(len(seq)), key=seq.__getitem__)
+
+
+def worker(cell_id, args):
+    swc_file = args['swc_file']
+    mech_file = args['mech_file']
+    final_pop = args['final_pop']
+    default_parameters = args['default_parameters']
+    evaluator = args['evaluator']
+    I = args['I']
+    stim_dur = args['stim_dur']
+    stim_start = args['stim_start']
+    feature_names = args['feature_names']
+    feature_reference_values = args['feature_reference_values']
+    err_max = args['err_max']
+    verbose = args['verbose']
+    individual = final_pop[cell_id]
+    params_file = '/tmp/individual_%03d.json' % cell_id
+    dump_parameters(individual, default_parameters, evaluator, params_file)
+    stim_end = stim_start + stim_dur
+
+    n_steps = len(I)
+    for i in range(n_steps):
+        good = True
+        cell_name = 'individual_%03d_%d' % (cell_id,i)
+        recorders = inject_current_step(I[i], swc_file, mech_file, params_file, stim_start, stim_dur, cell_name, do_plot=False)
+        h('forall delete_section()')
+        trace = {'T': recorders['t'], 'V': recorders['Vsoma'], 'stim_start': [stim_start], 'stim_end': [stim_end]}
+        feature_values = efel.getFeatureValues([trace],feature_names)
+        for name in feature_names:
+            if feature_values[0][name] is None:
+                if verbose:
+                    print(colors.red('[%d] %s is None.' % (cell_id,name)))
+                good = False
+                continue
+            m = feature_reference_values[name][i][0]
+            s = feature_reference_values[name][i][1]
+            err = np.abs(np.mean(feature_values[0][name]) - m) / s
+            if err > err_max:
+                if verbose:
+                    print(colors.red('[%d] %s = %g' % (cell_id,name,err)))
+                good = False
+            elif verbose:
+                print(colors.green('[%d] %s = %g' % (cell_id,name,err)))
+
+        if not good:
+            print('Individual ' + colors.red('%03d does not match' % (cell_id+1)) + ' the requisites')
+            return False
+
+    print('Individual ' + colors.green('%03d matches'%(cell_id+1)) + ' the requisites')
+    return True
+
+
+if __name__ == '__main__':
+
     parser = arg.ArgumentParser(description='Select cells that match a certain quality threshold.')
     parser.add_argument('folder', type=str, action='store', help='folder containing the results of the optimization')
     parser.add_argument('-t', '--threshold', default=3, type=int, help='threshold on the number of STDs to accept a solution')
     parser.add_argument('--features', default='all', type=str, help='comma-separated list of features to consider (default: all)')
     parser.add_argument('--ignore', default=None, type=str, help='comma-separated list of features to ignore')
     parser.add_argument('-a', '--all', action='store_true', help='process all solutions (potentially very time consuming)')
+    parser.add_argument('-p', '--parallel', action='store_true', help='use SCOOP to integrate the individuals in a parallel fashion')
     parser.add_argument('-v', '--verbose', action='store_true', help='be verbose')
     args = parser.parse_args(args=sys.argv[1:])
 
@@ -135,8 +195,17 @@ def main():
 
     do_all = args.all
     verbose = args.verbose
-    
-    colors = ColorFactory()
+
+    map_func = map
+    if args.parallel:
+        if not do_all:
+            print('Ignoring the --parallel option.')
+        else:
+            try:
+                from scoop import futures
+                map_func = futures.map
+            except:
+                print('SCOOP not found: will run serially.')
 
     final_pop = np.array(pickle.load(open(folder + '/final_population.pkl', 'rb'), encoding='latin1'))
     # remove duplicate individuals
@@ -185,72 +254,59 @@ def main():
         else:
             print('Individual ' + colors.red('%03d'%(i+1)) + ' of the hall-of-fame ' + colors.red('does not match') + ' the requisites.')
 
+    data = {'good_individuals_hof': good_individuals_hof,
+            'good_population': hof[good_individuals_hof,:],
+            'err_max': err_max,
+            'features': features_to_consider,
+            'ignored_features': features_to_ignore}
 
     if do_all:
-        swc_file = folder + '/110203_b_x40_4.converted.swc'
-        mech_file = folder + '/mechanisms.json'
-
-        do_plot = False
-
-        good_individuals = []
-
+        good_individuals_also_in_hof = []
         for i,individual in enumerate(final_pop):
             idx = (individual == hof).all(axis=1).nonzero()[0]
             if len(idx) > 0:
                 if idx[0] in good_individuals_hof:
-                    good_individuals.append(idx)
+                    good_individuals_also_in_hof.append(i)
                     print('Individual ' + colors.green('%03d'%(i+1)) + ' is in the hall-of-fame (#{}) and '.format(idx[0]+1) + \
                           colors.green('matches') + ' the requisites.')
                 else:
                     print('Individual ' + colors.red('%03d'%(i+1)) + ' is in the hall-of-fame (#{}) and '.format(idx[0]+1) + \
                           colors.red('does not match') + ' the requisites.')
-                continue
-            params_file = '/tmp/individual_%03d.json' % i
-            dump_parameters(individual, default_parameters, evaluator, params_file)
-            if verbose:
-                print('----------------------------------------')
-                print('Individual %02d' % (i+1))
-            for j,step in enumerate(protocols):
-                cell_name = 'individual_%03d_%d' % (i,j)
-                I = protocols[step]['stimuli'][0]['amp'] * 1e3
-                dur = protocols[step]['stimuli'][0]['duration']
-                stim_start = protocols[step]['stimuli'][0]['delay']
-                stim_end = stim_start+dur
-                if verbose:
-                    print('  %s' % step)
-                recorders = inject_current_step(I, swc_file, mech_file, params_file, stim_start, dur, cell_name, do_plot)
-                h('forall delete_section()')
-                trace = {'T': recorders['t'], 'V': recorders['Vsoma'], 'stim_start': [stim_start], 'stim_end': [stim_end]}
-                feature_names = [key for key in features[step]['soma']]
-                feature_values = efel.getFeatureValues([trace],feature_names)
-                good = True
-                for name in feature_names:
-                    m = features[step]['soma'][name][0]
-                    s = features[step]['soma'][name][1]
-                    try:
-                        err = np.abs(np.mean(feature_values[0][name]) - m) / s
-                        if err > err_max:
-                            if not name in features_to_ignore:
-                                good = False
-                                if verbose:
-                                    print(colors.red('    %s = %g' % (name,err)))
-                            elif verbose:
-                                print(colors.yellow('    %s = %g' % (name,err)))
-                        elif verbose:
-                            print(colors.green('    %s = %g' % (name,err)))
-                    except:
-                        good = False
-                        if verbose:
-                            print(colors.red('    %s = None' % name))
-                if not good:
-                    break
-            if good:
-                good_individuals.append(i)
-                print('Individual ' + colors.green('%03d'%(i+1)) + colors.green(' matches') + ' the requisites')
-            else:
-                print('Individual ' + colors.red('%03d'%(i+1)) + colors.red(' does not match') + ' the requisites')
 
-        print('Good invidividuals: ', good_individuals)
-    
-if __name__ == '__main__':
-    main()
+        swc_file = glob.glob(folder + '/*.converted.swc')[0]
+        mech_file = folder + '/mechanisms.json'
+
+        k = list(protocols.keys())[0]
+        feature_names = [key for key in features[k]['soma'] if not key in features_to_ignore and \
+                         (len(features_to_consider) == 0 or key in features_to_consider)]
+        feature_reference_values = {feature: [] for feature in feature_names}
+        for feature in feature_names:
+            for step in protocols:
+                feature_reference_values[feature].append(features[step]['soma'][feature])
+        stim_dur = protocols[k]['stimuli'][0]['duration']
+        stim_start = protocols[k]['stimuli'][0]['delay']
+        I = [proto['stimuli'][0]['amp']*1e3 for _,proto in protocols.items()]
+        idx = argsort(I)
+        I = [I[i] for i in idx]
+        feature_reference_values = {feature: [feature_reference_values[feature][i] for i in idx] for feature in feature_names}
+
+        args = {'swc_file': swc_file,
+                'mech_file': mech_file,
+                'final_pop': final_pop,
+                'default_parameters': default_parameters,
+                'evaluator': evaluator,
+                'I': I,
+                'stim_dur': stim_dur,
+                'stim_start': stim_start,
+                'feature_names': feature_names,
+                'feature_reference_values': feature_reference_values,
+                'err_max': err_max,
+                'verbose': verbose}
+
+        individuals = [i for i in range(n_individuals) if i not in good_individuals_also_in_hof]
+        good = list(map_func(lambda i: worker(i,args), individuals))
+        good_individuals = [ind for ind,gd in zip(individuals,good) if gd]
+        data['good_individuals'] = good_individuals
+        data['good_population'] = np.concatenate((data['good_population'], final_pop[good_individuals,:]))
+
+    pickle.dump(data, open(folder + '/good_population_%d_STDs.pkl' % err_max, 'wb'))
