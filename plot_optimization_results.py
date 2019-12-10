c214cec7d3d1ef7f992f7aded652db29f950c624
Daniele Linaro <danielelinaro@gmail.com>
2019-12-10 20:03:19 +0100

    Adapted plot_optimization_results.py to multiple recording sites

--- plot_optimization_results.py
+++ plot_optimization_results.py
@@ -43,13 +43,14 @@ def plot_summary(target_features,hall_of_fame,final_pop,evaluator,responses,indi
     pop_size = len(final_pop)
     n_params = len(evaluator.param_names)
 
-    params = final_pop.transpose().copy()
+    params = np.abs(final_pop.transpose().copy())
     bounds = np.zeros((n_params,2))
     for i in range(n_params):
         bounds[i,:] = evaluator.cell_model.params[evaluator.param_names[i]].bounds
+    bounds = np.abs(bounds)
     idx = np.argsort(bounds[:,1])
     params_median = np.median(params,1)
-    best_params = np.array(hall_of_fame[individual])[idx]
+    best_params = np.abs(np.array(hall_of_fame[individual])[idx])
     params = params[idx,:]
     params_median = params_median[idx]
     bounds = bounds[idx,:]
@@ -58,70 +59,93 @@ def plot_summary(target_features,hall_of_fame,final_pop,evaluator,responses,indi
     features = {}
     features_std_units = {}
     all_feature_names = []
+    proto_names = target_features.keys()
     for proto,target_feature in target_features.items():
-        feature_names = target_feature['soma'].keys()
-        stim_start = evaluator.fitness_protocols[proto].stimuli[0].step_delay
-        stim_end = stim_start + evaluator.fitness_protocols[proto].stimuli[0].step_duration
-        trace = {'T': responses[individual][proto+'.soma.v']['time'],
-                 'V': responses[individual][proto+'.soma.v']['voltage'],
-                 'stim_start': [stim_start], 'stim_end': [stim_end]}
-        feature_values = efel.getFeatureValues([trace],feature_names)[0]
-        features[proto] = {k: [np.mean(v),np.std(v)] if v is not None else [None,None] \
-                           for k,v in feature_values.items()}
-        features_std_units[proto] = {k: np.abs(target_feature['soma'][k][0]-np.mean(v)) / target_feature['soma'][k][1] \
-                                     if v is not None else None for k,v in feature_values.items()}
-        if verbose:
-            print('%s:' % proto)
-        for name,values in target_feature['soma'].items():
-            if not name in all_feature_names:
-                all_feature_names.append(name)
+        site_names = target_feature.keys()
+        features[proto] = {}
+        features_std_units[proto] = {}
+        for site in target_feature:
+            feature_names = target_feature[site].keys()
+            stim_start = evaluator.fitness_protocols[proto].stimuli[0].step_delay
+            stim_end = stim_start + evaluator.fitness_protocols[proto].stimuli[0].step_duration
+            trace = {'T': responses[individual]['{}.{}.v'.format(proto,site)]['time'],
+                     'V': responses[individual]['{}.{}.v'.format(proto,site)]['voltage'],
+                     'stim_start': [stim_start], 'stim_end': [stim_end]}
+            feature_values = efel.getFeatureValues([trace],feature_names)[0]
+            features[proto][site] = {k: [np.mean(v),np.std(v)] if v is not None else [None,None] \
+                                     for k,v in feature_values.items()}
+            features_std_units[proto][site] = {k: np.abs(target_feature[site][k][0]-np.mean(v)) / target_feature[site][k][1] \
+                                               if v is not None else None for k,v in feature_values.items()}
             if verbose:
-                if features[proto][name][0] is None:
-                    print('\t%s: model: None. data: %g +- %g (std/mean: %g)' %
-                          (name,values[0],values[1],values[1]/np.abs(values[0])))
-                else:
-                    print('\t%s: model: %g (%g std from data mean). data: %g +- %g (std/mean: %g)' %
-                          (name,features[proto][name][0],features_std_units[proto][name],
-                           values[0],values[1],values[1]/np.abs(values[0])))
+                print('{}.{}:'.format(proto,site))
+            for name,values in target_feature[site].items():
+                if not name in all_feature_names:
+                    all_feature_names.append(name)
+                if verbose:
+                    if features[proto][site][name][0] is None:
+                        print('\t%s: model: None. data: %g +- %g (std/mean: %g)' %
+                              (name,values[0],values[1],values[1]/np.abs(values[0])))
+                    else:
+                        print('\t%s: model: %g (%g std from data mean). data: %g +- %g (std/mean: %g)' %
+                              (name,features[proto][site][name][0],features_std_units[proto][site][name],
+                               values[0],values[1],values[1]/np.abs(values[0])))
 
-    n_steps = len(responses[individual])
+    n_proto = len(proto_names)
+    n_sites = len(site_names)
 
     if dump:
-        fid = open('%s_individual_%d_errors.csv'%(os.path.basename(os.path.abspath('.')),individual),'w')
+        fid = open('{}_individual_{}_errors.csv'.format(os.path.basename(os.path.abspath('.')),individual), 'w')
+        fid.write('feature name')
+        for proto in proto_names:
+            for site in site_names:
+                fid.write(',{}.{}'.format(proto,site))
+        fid.write('\n')
         for k in all_feature_names:
-            fid.write('%s,' % k)
-            for i in range(1,n_steps+1):
-                try:
-                    fid.write('%f,' % features_std_units['Step%d'%i][k])
-                except:
-                    fid.write(',')
-            fid.write('%f\n' % features_std_units['Step%d'%i][k])
+            fid.write(k)
+            for proto in proto_names:
+                for site in site_names:
+                    fid.write(',{}'.format(features_std_units[proto][site][k]))
+            fid.write('\n')
         fid.close()
 
-    plt.figure(figsize=(6,5))
+    plt.figure(figsize=(6,5+(n_sites-1)*2))
     plt.axes([0.01,0.75,0.98,0.25])
     offset = 0
     before = 250
     after = 200
     dx = 100
     dy = 50
+    colors = 'krgbcmy'
+    cmap = {site: col for site,col in zip(site_names,colors)}
     if dump:
-        fid = open('%s_individual_%d_traces.csv'%(os.path.basename(os.path.abspath('.')),individual),'w')
-    for resp in responses[individual].values():
-        # this is because of the variable time-step integration
-        start = np.max((0,np.where(resp['time'] > stim_start-before)[0][0] - 1))
-        stop = np.where(resp['time'] < stim_end+after)[0][-1] + 2
-        idx = np.arange(start,stop)
-        plt.plot(resp['time'][idx],resp['voltage'][idx]+offset,'k',lw=1)
+        fid = open('{}_individual_{}_traces.csv'.format(os.path.basename(os.path.abspath('.')),individual), 'w')
+    for proto in proto_names:
+        l_min = 1e6
+        l_max = -1e6
+        for site in site_names:
+            key = '{}.{}.v'.format(proto,site)
+            resp = responses[individual][key]
+            # this is because of the variable time-step integration
+            start = np.max((0,np.where(resp['time'] > stim_start-before)[0][0] - 1))
+            stop = np.where(resp['time'] < stim_end+after)[0][-1] + 2
+            idx = np.arange(start,stop)
+            plt.plot(resp['time'][idx],resp['voltage'][idx]+offset,cmap[site],lw=1)
+            if dump:
+                fid.write('{}.{}.time'.format(proto,site))
+                for v in resp['time'][idx]:
+                    fid.write(',{:.4f}'.format(v-resp['time'][idx[0]]))
+                fid.write('\n')
+                fid.write('{}.{}.voltage'.format(proto,site))
+                for v in resp['voltage'][idx]:
+                    fid.write(',{:.4f}'.format(v))
+                fid.write('\n')
+            if np.min(resp['voltage']) < l_min:
+                l_min = np.min(resp['voltage'])
+            if np.max(resp['voltage']) > l_max:
+                l_max = np.max(resp['voltage'])
         old_offset = offset
-        offset += np.diff([np.min(resp['voltage']),np.max(resp['voltage'])])[0] + 5
-        if dump:
-            for v in resp['time'][idx]:
-                fid.write('%f,' % (v-resp['time'][idx[0]]))
-            fid.write('\n')
-            for v in resp['voltage'][idx]:
-                fid.write('%f,' % v)
-            fid.write('\n')
+        offset += np.max(np.diff([l_min,l_max])[0] + 5)
+
     if dump:
         fid.close()
     
@@ -136,45 +160,50 @@ def plot_summary(target_features,hall_of_fame,final_pop,evaluator,responses,indi
     plt.axis('off')
 
     fnt = 8
-    offset = 0.175
-    space = 0.03
-    dx = (0.97 - offset - (n_steps-1)*space)/n_steps
+    offset = {'x': 0.175, 'y': 0.4}
+    space = {'x': 0.03, 'y': 0.06}
+    dx = (0.97 - offset['x'] - (n_proto-1)*space['x'])/n_proto
+    dy = (0.7 - offset['y'] - (n_sites-1)*space['y'])/n_sites
     all_feature_names = sorted(all_feature_names)
     n_features = len(all_feature_names)
     Y = list(range(n_features,0,-1))
-    dy = 0.3
     green = [0,.7,.3]
-    for i,(stepnum,feat) in enumerate(features_std_units.items()):
-        ax = plt.axes([offset+i*(dx+space),0.45,dx,0.25])
-        X = np.zeros(n_features)
-        for k,v in feat.items():
-            idx, = np.where([k == f for f in all_feature_names])
-            X[idx] = v
-        for x,y in zip(X,Y):
-            ax.add_patch(Rectangle((0,y-dy),x,2*dy,\
-                                   edgecolor=green,facecolor=green,linewidth=1))
-        plt.plot([5,5],[np.min(Y)-1,np.max(Y)+1],'r--',lw=1)
-        plt.title(stepnum,fontsize=fnt)
-        plt.xlabel('Objective value (# std)',fontsize=fnt)
-        
-        if np.max(X) > 31:
-            dtick = 10
-        elif np.max(X) > 14:
-            dtick = 5
-        else:
-            dtick = 2
+    for i,proto in enumerate(proto_names):
+        for j,site in enumerate(site_names):
+            ax = plt.axes([offset['x']+i*(dx+space['x']),offset['y']+j*(dy+space['y']),dx,dy])
+            feat = features_std_units[proto][site]
+            X = np.zeros(n_features)
+            for k,v in feat.items():
+                idx, = np.where([k == f for f in all_feature_names])
+                X[idx] = v
+            for x,y in zip(X,Y):
+                ax.add_patch(Rectangle((0,y-0.3),x,2*0.3,\
+                                       edgecolor=green,facecolor=green,linewidth=1))
+            ax.plot([5,5],[np.min(Y)-1,np.max(Y)+1],'r--',lw=1)
+            ax.set_title('{}.{}'.format(proto,site),fontsize=fnt)
+            if np.max(X) > 31:
+                dtick = 10
+            elif np.max(X) > 14:
+                dtick = 5
+            elif np.max(X) > 5:
+                dtick = 2
+            else:
+                dtick = 1
 
-        plt.xticks(np.arange(0,np.ceil(np.nanmax(X))+1,dtick),fontsize=fnt)
-    
-        if i == 0:
-            plt.yticks(Y,all_feature_names,fontsize=fnt-3)
-        else:
-            plt.yticks(Y,[])
+            plt.xticks(np.arange(0,np.max([6,np.ceil(np.nanmax(X))+1]),dtick),fontsize=fnt)
+
+            if j == 0:
+                plt.xlabel('Objective value (# std)',fontsize=fnt)
+            if i == 0:
+                plt.yticks(Y,all_feature_names,fontsize=fnt-3)
+            else:
+                plt.yticks(Y,[])
         
-        plt.axis([0,np.max([5.1,np.ceil(np.nanmax(X))]),np.min(Y)-1,np.max(Y)+1])
+            ax.set_xlim([0,np.max([5.1,np.ceil(np.nanmax(X))])])
+            ax.set_ylim([np.min(Y)-1,np.max(Y)+1])
 
     blue = [.9,.9,1]
-    ax = plt.axes([0.25,0.05,0.72,0.3])
+    ax = plt.axes([0.25,0.05,0.72,0.25])
     dy = 0.3
     plot = plt.semilogx
     for y0,(par,best_par,m,b) in enumerate(zip(params,best_params,params_median,bounds)):
@@ -187,14 +216,12 @@ def plot_summary(target_features,hall_of_fame,final_pop,evaluator,responses,indi
         ax.add_patch(Rectangle((b[0],y0-dy),b[1]-b[0],2*dy,\
                                edgecolor=blue,facecolor=blue,linewidth=1))
 
-
-    outfile = os.path.basename(os.path.abspath('.')) + '_individual_%d.pdf' % individual
+    outfile = os.path.basename(os.path.abspath('.')) + '_individual_{}.pdf'.format(individual)
 
     plt.xlim([1e-6,1500])
     plt.ylim([-1,n_params])
     plt.yticks(np.arange(n_params),param_names,fontsize=fnt-3)
     plt.savefig(outfile)
-    #plt.show()
 
 
 def main():
