d98aac28d18deba821e7c8c776a91f1416f8c56b
Daniele Linaro <danielelinaro@gmail.com>
2020-04-22 12:19:01 +0200

    measure_impedance.py now accepts pickle files

--- measure_impedance.py
+++ measure_impedance.py
@@ -12,6 +12,8 @@ import matplotlib.colors as colors
 
 import neuron
 import dlutils.cell as cu
+from dlutils import utils
+from dlutils.utils import individuals_from_pickle
 
 import btmorph
 
@@ -54,9 +56,9 @@ def measure_impedance(cell, segment, stim_pars):
     return R
 
 
-def worker(segment_num, segment_group, stim_pars, swc_file, parameters, mechanisms, replace_axon, add_axon_if_missing, passive_cell):
+def worker(segment_num, segment_group, stim_pars, swc_file, parameters, mechanisms, replace_axon, add_axon_if_missing, passive_cell, cell_id=0):
 
-    cell_name = '{}_{:03d}'.format(segment_group, segment_num)
+    cell_name = '{}_{:03d}_{}'.format(segment_group, segment_num, cell_id)
 
     cell = cu.Cell(cell_name, swc_file, parameters, mechanisms)
     cell.instantiate(replace_axon, add_axon_if_missing, force_passive=passive_cell)
@@ -178,10 +180,10 @@ if __name__ == '__main__':
     
     parser = arg.ArgumentParser(description='Measure the impedance of each compartment in a neuron model.')
     parser.add_argument('I', type=float, action='store', default=-50, nargs='?', help='current value in pA')
-
+    parser.add_argument('-P','--pickle-file', type=str, default='', help='Pickle file containing the parameters of a population of individuals')
+    parser.add_argument('-e','--evaluator-file', type=str, default='evaluator.pkl', help='Pickle file containing the evaluator')
     parser.add_argument('-f','--swc-file', type=str, help='SWC file defining the cell morphology', required=True)
-    parser.add_argument('-p','--params-file', type=str, default=None, required=True,
-                        help='JSON file containing the parameters of the cell')
+    parser.add_argument('-p','--params-files', type=str, default='', help='JSON file(s) containing the parameters of the cell(s)')
     parser.add_argument('-m','--mech-file', type=str, default=None,
                         help='JSON file containing the mechanisms to be inserted into the cell')
     parser.add_argument('-c','--config-file', type=str, default=None,
@@ -211,16 +213,9 @@ if __name__ == '__main__':
             map_fun = map
             print('SCOOP not found: will run sequentially')
 
-    from dlutils import utils
-
     if not os.path.isfile(args.swc_file):
         print('{}: {}: no such file.'.format(progname,args.swc_file))
         sys.exit(1)
-
-    if not os.path.isfile(args.params_file):
-        print('{}: {}: no such file.'.format(progname,args.params_file))
-        sys.exit(1)
-    parameters = json.load(open(args.params_file, 'r'))
 
     if args.mech_file is not None:
         if not os.path.isfile(args.mech_file):
@@ -237,6 +232,24 @@ if __name__ == '__main__':
             sys.exit(1)
         mechanisms = utils.extract_mechanisms(args.config_file, args.cell_name)
         cell_name = args.cell_name
+
+    if '*' in args.params_files:
+        import glob
+        params_files = glob.glob(args.params_files)
+    else:
+        params_files = args.params_files.split(',')
+    if params_files[0] == '':
+        params_files = []
+
+    if args.pickle_file == '':
+        population = [json.load(open(params_file,'r')) for params_file in params_files]
+        working_dir = os.path.split(params_files[0])[0]
+    else:
+        if len(params_files) > 0:
+            print('You cannot simultaneously specify parameter and pickle files.')
+            sys.exit(1)
+        population = individuals_from_pickle(args.pickle_file, args.config_file, cell_name, args.evaluator_file)
+        working_dir = os.path.split(args.pickle_file)[0]
 
     if cell_name[-1] == '_':
         cell_name = cell_name[:-1]
@@ -289,71 +302,79 @@ if __name__ == '__main__':
     swc_file = args.swc_file
     stim_pars = {'delay': args.delay, 'duration': args.dur, 'amplitude': args.I * 1e-3}
 
-    cell = cu.Cell('CA3_cell', swc_file, parameters, mechanisms)
-    cell.instantiate(replace_axon, add_axon_if_missing, force_passive=passive_cell)
 
-    R = {'soma': np.array([measure_impedance(cell, cell.somatic_segments[0]['seg'], stim_pars)])}
+    for i,parameters in enumerate(population):
+        cell = cu.Cell('CA3_cell_{}'.format(i), swc_file, parameters, mechanisms)
+        cell.instantiate(replace_axon, add_axon_if_missing, force_passive=passive_cell)
 
-    centers = {'soma': np.array([cell.somatic_segments[0]['center']])}
-    centers['basal'] = np.array([seg['center'] for seg in cell.basal_segments])
-    centers['apical'] = np.array([seg['center'] for seg in cell.apical_segments])
+        R = {'soma': np.array([measure_impedance(cell, cell.somatic_segments[0]['seg'], stim_pars)])}
 
-    diameters = {'soma': np.array([cell.somatic_segments[0]['sec'].diam])}
-    diameters['basal'] = np.array([seg['sec'].diam for seg in cell.basal_segments])
-    diameters['apical'] = np.array([seg['sec'].diam for seg in cell.apical_segments])
+        centers = {'soma': np.array([cell.somatic_segments[0]['center']])}
+        centers['basal'] = np.array([seg['center'] for seg in cell.basal_segments])
+        centers['apical'] = np.array([seg['center'] for seg in cell.apical_segments])
 
-    areas = {'soma': np.array([cell.somatic_segments[0]['area']])}
-    areas['basal'] = np.array([seg['area'] for seg in cell.basal_segments])
-    areas['apical'] = np.array([seg['area'] for seg in cell.apical_segments])
+        diameters = {'soma': np.array([cell.somatic_segments[0]['sec'].diam])}
+        diameters['basal'] = np.array([seg['sec'].diam for seg in cell.basal_segments])
+        diameters['apical'] = np.array([seg['sec'].diam for seg in cell.apical_segments])
 
-    print('Somatic impedance: {:.2f} MOhm.'.format(R['soma'][0]))
+        areas = {'soma': np.array([cell.somatic_segments[0]['area']])}
+        areas['basal'] = np.array([seg['area'] for seg in cell.basal_segments])
+        areas['apical'] = np.array([seg['area'] for seg in cell.apical_segments])
+
+        print('Somatic impedance: {:.2f} MOhm.'.format(R['soma'][0]))
+
+        N = {'basal': len(cell.basal_segments), 'apical': len(cell.apical_segments)}
+        print('The cell has {} basal and {} apical segments.'.format(N['basal'], N['apical']))
     
-    N = {'basal': len(cell.basal_segments), 'apical': len(cell.apical_segments)}
-    print('The cell has {} basal and {} apical segments.'.format(N['basal'], N['apical']))
-    
-    idx = {k: np.arange(v) for k,v in N.items()}
-    if args.trial_run:
-        idx = {k: np.random.choice(v, size=10, replace=False) for k,v in idx.items()}
+        idx = {k: np.arange(v) for k,v in N.items()}
+        if args.trial_run:
+            idx = {k: np.random.choice(v, size=10, replace=False) for k,v in idx.items()}
 
-    neuron.h('forall delete_section()')
+        neuron.h('forall delete_section()')
 
-    for dend_type in idx:
-        centers[dend_type] = centers[dend_type][idx[dend_type],:]
-        diameters[dend_type] = diameters[dend_type][idx[dend_type]]
-        areas[dend_type] = areas[dend_type][idx[dend_type]]
-        fun = lambda num: worker(num, dend_type, stim_pars, swc_file, parameters,
-                                 mechanisms, replace_axon, add_axon_if_missing,
-                                 passive_cell)
-        R[dend_type] = np.array(list(map_fun(fun, idx[dend_type])))
+        for dend_type in idx:
+            centers[dend_type] = centers[dend_type][idx[dend_type],:]
+            diameters[dend_type] = diameters[dend_type][idx[dend_type]]
+            areas[dend_type] = areas[dend_type][idx[dend_type]]
+            fun = lambda num: worker(num, dend_type, stim_pars, swc_file, parameters,
+                                     mechanisms, replace_axon, add_axon_if_missing,
+                                     passive_cell, i)
+            R[dend_type] = np.array(list(map_fun(fun, idx[dend_type])))
 
-    data = {
-        'N': N,
-        'centers': centers,
-        'diameters': diameters,
-        'areas': areas,
-        'morphology': np.loadtxt(swc_file),
-        'R': R,
-        'swc_file': swc_file,
-        'stim_delay': args.delay,
-        'stim_dur': args.dur,
-        'stim_amp': args.I * 1e-3,
-        'segment_indexes': idx,
-        'parameters': parameters,
-        'mechanisms': mechanisms,
-        'replace_axon': replace_axon,
-        'add_axon_if_missing': add_axon_if_missing,
-        'cell_name': cell_name,
-        'passive_cell': passive_cell,
-        'params_file': args.params_file
-    }
+        data = {
+            'N': N,
+            'centers': centers,
+            'diameters': diameters,
+            'areas': areas,
+            'morphology': np.loadtxt(swc_file),
+            'R': R,
+            'swc_file': swc_file,
+            'stim_delay': args.delay,
+            'stim_dur': args.dur,
+            'stim_amp': args.I * 1e-3,
+            'segment_indexes': idx,
+            'parameters': parameters,
+            'mechanisms': mechanisms,
+            'replace_axon': replace_axon,
+            'add_axon_if_missing': add_axon_if_missing,
+            'cell_name': cell_name,
+            'passive_cell': passive_cell
+        }
 
-    if args.config_file is not None:
-        data['config_file'] = args.config_file
-    else:
-        data['mech_file'] = args.mech_file
+        if args.config_file is not None:
+            data['config_file'] = args.config_file
+        else:
+            data['mech_file'] = args.mech_file
 
-    outfile = cell_name + '_impedance_' + os.path.splitext(args.params_file)[0] + '_' + args.model_type + '.pkl'
-    pickle.dump(data, open(outfile, 'wb'))
+        if len(params_files) > 0:
+            data['params_file'] = args.params_files[i]
+            suffix = os.path.splitext(params_files[i])[0]
+        else:
+            data['pickle_file'] = args.pickle_file
+            data['individual'] = i
+            suffix = 'pkl_individual_{}'.format(i)
 
-    plot(outfile)
+        outfile = cell_name + '_impedance_' + suffix + '_' + args.model_type + '.pkl'
+        pickle.dump(data, open(outfile, 'wb'))
+        plot(outfile)
 
